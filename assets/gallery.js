fdc21b12eb16d4f646b87d9ef31c48c522d131bb
István Szmozsánszky <git@flaki.hu>
2019-01-09 17:11:58 +0100

    Initial image zoom / gallery support

---
+++ assets/gallery.js
@@ -0,0 +1,34 @@
+setTimeout(() => {
+  const dialog = document.querySelector('dialog.gallery')
+
+  Array.from(
+    document.querySelectorAll('article li>img')
+  ).forEach(img => img.addEventListener('click', (e) => {
+    const src = e.target.src
+    const alt = e.target.alt
+    const aspect = img.width/img.height
+    const container = document.querySelector('.gallery div')
+
+    container.innerHTML = ''
+    container.insertAdjacentHTML('beforeend', `<img src="${src}" alt="${alt.replace(/\"/g,'&quot;')}" /><label>${alt}</label>`)
+
+    let cw = aspect < 1 ? `${aspect * 90}vmin` : `90vmin`
+    let ch = aspect < 1 ? `90vmin` : `${90 / aspect}vmin`
+    container.style.width = cw
+    container.style.height = ch
+
+    dialog.showModal()
+  }))
+
+  document.querySelector('#show').onclick = function() {
+    document.querySelector('.gallery div').innerHTML = ''
+    document.querySelector('.gallery div').insertAdjacentHTML('beforeend', '<img src="http://blog-local.flak.is/img/africa/landcruiser.jpg" alt="" /><label>Chrischa was well-prepared: his Land Cruiser had a fold-out rooftop tent. Not only that it took maybe a minute to set up (and tear down), but also perfect vantage point and protection against predators!</label>')
+    dialog.showModal()
+  }
+
+  document.querySelector('dialog.gallery button.close').onclick = function() {
+    dialog.close()
+  }
+  /* polyfill */
+  dialogPolyfill.registerDialog(dialog)
+}, 0)
